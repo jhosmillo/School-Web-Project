1b064ef3e4d53f00d907711eb3674aada7d98aa6
jhosmillo <55266269+jhosmillo@users.noreply.github.com>
2019-10-29 17:37:54 -0700

    update

---
+++ js/select.js
@@ -0,0 +1,18 @@
+document.getElementById('primaryFunction').addEventListener('change', function () {
+								
+	var pf =document.getElementById('primaryFunction').value;
+	var c = document.getElementById('primaryFunction');
+	var x = c.options.length;
+	document.getElementById('secondaryFunction').value="";
+	for(a=0; a<x;a++){
+		document.getElementById('secondaryFunction').options[a].style.display = "block";
+	}
+	for(i=0; i<x;i++){
+		var sf = document.getElementById('secondaryFunction').options[i].value;
+		if(sf==pf){
+			document.getElementById('secondaryFunction').options[i].style.display = "none";
+		}
+	}
+	
+
+});
